f273df56963889141cae492d5609d6385cbdd410
Josh Mallery <josh.mallery@gmail.com>
2022-03-03 11:39:15 -0700

    Add PsuedoCode

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+var main = document.querySelector('main');
+var changeGame = document.querySelector('.change-game')
+//listener on MAIN container for standard or enhanced game containers
+//listener on container change-game
+
+main.addEventListener('click',);
+changeGame.addEventListener('click',);
+
+//eventhandler to listen for gametype selection click
+  //handler will need to
+    //will store the gametype selection value
+    //update
+      //hide game rules for normal and enhanced
+      //show game player icons
+
+//event handler to listen for game icon select
+  //will store the game icon selected and create game instantion
+    //hide icons not selected by the player or computer
+
+  //will then run game.determineWinner
+
+  //will say who won in the game Prompt container
+  //will update win tallies
+  //some type of delay and go back to game state prior to gameTypeSelection screen
+
+//eventhandler to act on change-game
+  //this will bascially take us back to the initial state prior to gameTypeSelection click
+  //make change-game button dissappear?
+
+
+
 //when the enhanced or standard game is clicked a new Game is instantiated
 //the instantiation takes in the gametype
 //something else then tells is the enhanced will be hidden or not
